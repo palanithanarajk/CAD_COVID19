a4b87352e503e0a6605f964beb670df3e352bac2
Palani Thanaraj Krishnan <palanithanaraj.k@gmail.com>
2020-07-04 15:47:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,10 +61,10 @@ def upload():
         f = request.files['file']
 
         # Save the file to ./uploads
-        #basepath = os.path.dirname(__file__)
-        #file_path = os.path.join(
-        #    basepath, 'uploads', secure_filename(f.filename))
-        #f.save(file_path)
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
 
         # Make prediction
         preds = model_predict(f, model)
